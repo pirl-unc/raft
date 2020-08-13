ab172c33233640f8f36f208f73a7aa25088e0fd0
Steven Patrick Vensko <spvensko@fc830-2-10.local>
2020-08-13 14:03:39 -0400

    Fixing typo.

--- .script/raft.py
+++ .script/raft.py
@@ -478,11 +478,8 @@ def setup_run_once(master_cfg):
     for dir in master_cfg['filesystem'].values():
         if os.path.isdir(dir): # Need to ensure dir isn't already in RAFT dir.
             print("Symlinking {} to {}...".format(dir, getcwd()))
-            dir_basename = os.path.basename(dir)
-            if dir == 'work'
-                dir_basename = 'work'
             try:
-                os.symlink(dir, pjoin(getcwd(), dir_basename))
+                os.symlink(dir, pjoin(getcwd(), os.path.basename(dir)))
             except:
                 print("{} already exists.".format(dir))
         else:
